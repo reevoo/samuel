024555fa102331385f2d0663ed7c9f3579f7f4dd
Jonny Arnold <jonny.arnold@reevoo.com>
2015-08-06 21:39:43 +0100

    Added a smattering of tests

---
+++ test/samuel/dispatchers_test.exs
@@ -0,0 +1,9 @@
+defmodule Samuel.DispatchersTest do
+  use ShouldI
+  alias Samuel.Dispatchers
+
+  should "do very little" do
+    # Not really sure what to put here.
+    # Do we need Dispatchers?
+  end
+end
