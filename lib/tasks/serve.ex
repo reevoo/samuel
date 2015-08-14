20f4c38f99b11573d6cf4845d3138c9d37efbdee
Jonny Arnold <jonny.arnold@reevoo.com>
2015-08-14 10:17:30 +0100

    Ooops 2

--- lib/tasks/serve.ex
+++ lib/tasks/serve.ex
@@ -1,7 +1,8 @@
 defmodule Mix.Tasks.Serve do
   use Mix.Task
 
-  def run(port) do
-    Samuel.API.Server.start(Integer.parse(port))
+  def run([port_str]) do
+    {port, _} = Integer.parse(port_str)
+    Samuel.API.Server.start(port)
   end
 end
