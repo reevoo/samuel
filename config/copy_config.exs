f8a018f1a7fed55f3a91dcb41ab4bc7b26463dcf
Jonny Arnold <jonny.arnold@reevoo.com>
2016-01-14 09:23:54 +0000

    Fix style errors

--- config/copy_config.exs
+++ config/copy_config.exs
@@ -4,8 +4,8 @@ config :samuel,
   guidelines_message: """
   *If you're new, read the [guidelines](https://github.com/reevoo/guidelines/blob/master/pull_requests.md).*
 
-  Good code is **tested**, **designed for change**, **easy to understand** and **able to cope when
-  things go wrong**.
+  Good code is **tested**, **designed for change**, **easy to understand** 
+  and **able to cope when things go wrong**.
 
   Good pull requests are **descriptive**, **small** and **short-lived**.
 
